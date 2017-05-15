86080e8868ffc79d1f636c4106f46bd9cfadd9a6
Chris Wald <wald.j.chris@gmail.com>
2017-05-15 18:40:02 -0500

    Update prototype endpoints.

--- link/test/bathrooms.config.js
+++ link/test/bathrooms.config.js
@@ -3,21 +3,39 @@
         "RequiredPoints": []
     },
 
+    "HttpMethod": "POST",
+
+    "RequestParameters": [
+        "ID"
+    ],
+
+    "DataGroups": [
+        "Result"
+    ],
+
     "DataSources": [
         {
-            "Type": "RequestParams",
-            "Properties": {
-                "Parameters": [
-                    "ID"
-                ]
-            }
-        },
-        {
             "Type": "Database",
+            "Parameters": [
+                {
+                    "Source": "RequestParameters",
+                    "SourceParameterName": "ID",
+                    "DestinationParameterName": "ID"
+                }
+            ],
             "Properties": {
                 "Database": "bathroomstatus",
                 "Query": "SELECT Name FROM Bathroom WHERE Bathroom.ID = ?"
+            },
+            "Result": {
+                "DataGroup": "Result",
+                "NameInGroup": "QueryResult"
             }
         }
-    ]
+    ],
+
+    "Result": {
+        "DataGroup": "Result",
+        "NameInGroup": "QueryResult"
+    }
 }
