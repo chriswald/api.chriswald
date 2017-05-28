2a7bf35b0975ed0a13b3eacc3a5bbfb71d3f33d9
Chris Wald <wald.j.chris@gmail.com>
2017-05-28 14:32:53 -0500

    Check security prerequisits.

--- link/apilink.php
+++ link/apilink.php
@@ -1,7 +1,11 @@
 <?php
 
 include_once "linkexception.php";
+include_once "linkapipoint.php";
 include_once "datagroups.php";
+
+include_once "securitysection.php";
+
 include_once "../auth/createconnection.php";
 include_once "../auth/user.php";
 
@@ -14,9 +18,35 @@ class ApiLink
         $this->_dataGroups = new DataGroups();
     }
 
-    public function ExecuteConfig($configFilename, &$httpStatusCode, &$responseObject)
+    public function ExecuteConfig(string $apiPoint, &$httpStatusCode, &$responseObject)
     {
+        try
+        {
+            $apiPoint = new LinkApiPoint($apiPoint);
+            if (PrerequisitsMet($apiPoint))
+            {
+                
+            }
+        }
+        catch (LinkException $e)
+        {
+            $httpStatusCode = $e->getStatusCode();
+            $responseObject = ["Error" => $e->getMessage()];
+        }
+    }
 
+    private function PrerequisitsMet(LinkApiPoint $apiPoint)
+    {
+        $securitySection = new SecuritySection($apiPoint->Config());
+        if (!$securitySection->ValidateUser(GetSessionToken()))
+        {
+            throw new LinkException(400, "Unauthorized");
+        }
+    }
+
+    private function GetSessionToken()
+    {
+        return $_POST["SessionToken"];
     }
 }
 
